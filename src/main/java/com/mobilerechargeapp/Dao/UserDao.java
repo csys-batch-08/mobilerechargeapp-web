b492f480fede1c9b1b88c7a5f7f76bda51e63300
Rahul R <rrahul24799@gmail.com>
2022-02-07 21:47:50 +0530

    project updated

--- src/main/java/com/mobilerechargeapp/Dao/UserDao.java
+++ src/main/java/com/mobilerechargeapp/Dao/UserDao.java
@@ -1,17 +1,16 @@
 package com.mobilerechargeapp.Dao;
 
-import java.sql.ResultSet;
+
 import java.util.List;
 
 import com.mobilerechargeapp.model.User;
 
 public interface UserDao {
 	public boolean insertUser(User user);
-	public User validiateUser(String Emailid, String password);
+	public User validiateUser(String emailid, String password);
 	public int findUserId(User user);
 	public  User findUser(String emailId);
 	public int updateuserWallet(User user);
-//	public int updateuserWallet(User user,double amount);
     public boolean rechargeWalletupdate(double planPrice, User user);
     public List<Object> history(int userId) ;
 }
--- src/main/java/com/mobilerechargeapp/daoimpl/UserDAOImpl.java
+++ src/main/java/com/mobilerechargeapp/daoimpl/UserDAOImpl.java
@@ -18,37 +18,49 @@ public class UserDAOImpl implements UserDao {
 
 	public boolean insertUser(User user) {
 		boolean flag = false;
-		Connection connection=ConnectionClass.getConnection();
-		String subQue = "select operator_id,operator_name from operator_details where operator_name=?";
+		Connection connection = ConnectionClass.getConnection();
 		PreparedStatement preparedStatement = null;
 		try {
-			preparedStatement = connection.prepareStatement(subQue);
-			preparedStatement.setString(1, user.getOperator().getOperatorname());
 			String insertQuery = "insert into userlogin (user_name,Email_id,phone_number,password,operator_Id) values(?,?,?,?,?)";
-
 			preparedStatement = connection.prepareStatement(insertQuery);
-
 			preparedStatement.setString(1, user.getUsername());
 			preparedStatement.setString(2, user.getEmailid());
 			preparedStatement.setLong(3, user.getPhonenumber());
 			preparedStatement.setString(4, user.getPassword());
-
 			preparedStatement.setInt(5, user.getOperator().getOperatorId1());
-
 			flag = preparedStatement.executeUpdate() > 0;
-
 		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionClass.close(connection, preparedStatement, null);
+		}
+		return flag;
+	}
+	
+	public int operatorName(String opertorName) {
+		Connection connection=ConnectionClass.getConnection();
+		String subQuery = "select operator_id,operator_name from operator_details where operator_name=?";
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		int opId = 0;
+		try {
+			preparedStatement = connection.prepareStatement(subQuery);
+			preparedStatement.setString(1,opertorName);
+			resultSet = preparedStatement.executeQuery();
+		if (resultSet.next()) {
+			opId = resultSet.getInt(1);
+		}
+		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		finally {
-			ConnectionClass.close(connection, preparedStatement, null);
+			ConnectionClass.close(connection, preparedStatement, resultSet);
 		}
-
-		return flag;
-
+		return opId;
+		
 	}
 
-	public User validiateUser(String Emailid, String password) {
+	public User validiateUser(String emailid, String password) {
 		String query = "select user_id,user_name,Email_id,phone_number,password,wallet,operator_id from userlogin where Email_id=? and password=?";
 		Connection connection = ConnectionClass.getConnection();
 		User user = null;
@@ -56,7 +68,7 @@ public class UserDAOImpl implements UserDao {
 		ResultSet resultSet = null;
 		try {
 			preparedStatement = connection.prepareStatement(query);
-			preparedStatement.setString(1, Emailid);
+			preparedStatement.setString(1, emailid);
 			preparedStatement.setString(2, password);
 			resultSet = preparedStatement.executeQuery();
 			OperatorDAOImpl operatordao = new OperatorDAOImpl();
@@ -64,7 +76,7 @@ public class UserDAOImpl implements UserDao {
 
 				Operator operator = operatordao.findOperator1(resultSet.getInt(7));
 
-				user = new User(resultSet.getString(2), Emailid, resultSet.getLong(4), password, resultSet.getDouble(6),
+				user = new User(resultSet.getString(2), emailid, resultSet.getLong(4), password, resultSet.getDouble(6),
 						operator);
 				user.setUserId(resultSet.getInt(1));
 			}
@@ -162,18 +174,12 @@ public class UserDAOImpl implements UserDao {
 	public boolean rechargeWalletupdate(double planPrice, User user) {
 		Connection connection=ConnectionClass.getConnection();
 		String query = "update userlogin set wallet=? where Email_id=?";
-		String getWalletquery = "select wallet from userlogin where Email_id=?";
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
 		boolean flag = false;
+		UserDAOImpl userDao=new UserDAOImpl();
+Double wallet=userDao.wallet(user);
 		try {
-			preparedStatement = connection.prepareStatement(getWalletquery);
-			preparedStatement.setString(1, user.getEmailid());
-			resultSet = preparedStatement.executeQuery();
-			double wallet = 0;
-			if (resultSet.next()) {
-				wallet = resultSet.getDouble(1);
-			}
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setDouble(1, wallet - planPrice);
 
@@ -191,6 +197,34 @@ public class UserDAOImpl implements UserDao {
 		return flag;
 
 	}
+	
+	public double  wallet(User user) {
+		Connection connection=ConnectionClass.getConnection();
+		String getWalletquery = "select wallet from userlogin where Email_id=?";
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		double	walletamount=0;
+		try {
+			preparedStatement = connection.prepareStatement(getWalletquery);
+			preparedStatement.setString(1, user.getEmailid());
+			resultSet = preparedStatement.executeQuery();
+			
+			if (resultSet.next()) {
+				walletamount = resultSet.getDouble(1);
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		finally {
+			ConnectionClass.close(connection, preparedStatement, resultSet);
+		}
+		
+		return walletamount;	
+	}
+	
+	
 
 	public User findUser(int userId) {
 		Connection connection=ConnectionClass.getConnection();
